efdd15364e64d2a8514fdfb73231027ca85a2385
Heavvenly <123119089+Heavvenly@users.noreply.github.com>
2025-01-01 15:18:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,653 @@
+const namesData = {
+  no: [{
+      type: "prompt",
+      text: "you forgot to select ur name lol"
+    },
+    {
+      type: "answer",
+      text: "reload the page"
+    }
+  ],
+  theo: [{
+      type: "prompt",
+      text: "wait you made this what are u doing"
+    },
+    {
+      type: "caption",
+      title: "This doesn't make like any sense",
+      text: "also hi whoever is looking at this cant you mind privacy jeez"
+    },
+    {
+      type: "image",
+      title: "Anyways heres an image on unsplash",
+      imageUrl: "https://images.unsplash.com/photo-1533673291136-38eb77758e50?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      type: "prompt",
+      text: "ok bye"
+    }
+  ],
+  azh: [{
+      type: "prompt",
+      text: "hi Azh!!!!!!!"
+    },
+    {
+      type: "answer",
+      text: "yk this is all ur fault, like we could have stopped with grey that wouldve been nice"
+    },
+    {
+      type: "prompt",
+      text: "good thing i used chatgpt so this didnt take too long (not true this has taken hours)"
+    },
+    {
+      type: "answer",
+      text: "ok anyways lets get on with it"
+    },
+    {
+      type: "prompt",
+      text: "This semester you talked about the love of your life…"
+    },
+    {
+      type: "caption",
+      title: "About the same amount as usual",
+      text: "which is so much"
+    },
+    {
+      type: "prompt",
+      text: "This semester your most uncomfortable moment was…"
+    },
+    {
+      type: "answer",
+      text: "Giving Matt his dirty iced chai but like it took 10 minutes"
+    },
+    {
+      type: "prompt",
+      text: "This semester your grades were…."
+    },
+    {
+      type: "answer",
+      text: "Idk u kinda always end up doing pretty well ¯\_(ツ)_/¯"
+    },
+    {
+      type: "prompt",
+      text: "This semester you were ____ neurodivergent!"
+    },
+    {
+      type: "answer",
+      text: "Very I mean very "
+    },
+    {
+      type: "prompt",
+      text: "Your top track of this year was…"
+    },
+    {
+      type: "caption",
+      title: "One of those songs about wanting men that are just no good",
+      text: "also stole this from u haha"
+    },
+    {
+      type: "image",
+      title: "Here’s an image I think describes you:",
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9d0ftG9bKqlZXZoTxnUutOQKGnp7lXjGNZw&s"
+    },
+    {
+      type: "prompt",
+      text: "Your quote of the semester is…"
+    },
+    {
+      type: "answer",
+      text: "Every single time you repeat a word like 15 times and then I say it also wrong and its the best"
+    },
+    {
+      type: "prompt",
+      text: "The craziest thing I learned about you this semester is…."
+    },
+    {
+      type: "answer",
+      text: "You were gonna move to Oregon again like huh stop it"
+    },
+    {
+      type: "prompt",
+      text: "This semester your most hated teacher was…"
+    },
+    {
+      type: "caption",
+      title: "Dan Jones",
+      text: "(but im pretty sure its really funny to make fun of marginalized groups actually)"
+    },
+    {
+      type: "prompt",
+      text: "This semester your most loved teacher was…"
+    },
+    {
+      type: "caption",
+      title: "Matt Sencill",
+      text: "least suprising thing ever"
+    },
+    {
+      type: "caption",
+      title: "Thanks for viewing your Semester Wrapped, see you next semester! ",
+      text: "(Maybe probably not) also dont click off yet theres one more slide ok"
+    },
+    {
+      type: "prompt",
+      text: "♥︎ u :)"
+    }
+  ],
+  sadie: [{
+      type: "prompt",
+      text: "hi Sadie!!!!!"
+    },
+    {
+      type: "answer",
+      text: "idk when I finished this but did I beat you to making holiday gifts"
+    },
+    {
+      type: "answer",
+      text: "anyways lets get into it"
+    },
+    {
+      type: "prompt",
+      text: "This semester you talked about the love of your life…"
+    },
+    {
+      type: "caption",
+      title: "This prompt isn’t for u",
+      text: "you know who it is for tho"
+    },
+    {
+      type: "prompt",
+      text: "This semester your grades were…."
+    },
+    {
+      type: "answer",
+      text: "Aced it I think probably"
+    },
+    {
+      type: "prompt",
+      text: "This semester you were ____ neurodivergent!"
+    },
+    {
+      type: "answer",
+      text: "Like a good amount of "
+    },
+    {
+      type: "prompt",
+      text: "Your top track of this year was…"
+    },
+    {
+      type: "answer",
+      text: "Neomi I still dont know what that is"
+    },
+    {
+      type: "prompt",
+      text: "Your quote of the semester is…"
+    },
+    {
+      type: "caption",
+      title: "Dan Jones !!!/???/…",
+      text: "also me tho"
+    },
+    {
+      type: "prompt",
+      text: "The craziest thing I learned about you this semester is…."
+    },
+    {
+      type: "answer",
+      text: "Ur birthday is June 30th (also is that right I literally never remember)"
+    },
+    {
+      type: "prompt",
+      text: "This semester your most hated teacher was…"
+    },
+    {
+      type: "caption",
+      title: "Laura Croft",
+      text: "she is so scary"
+    },
+    {
+      type: "prompt",
+      text: "This semester your most loved teacher was…"
+    },
+    {
+      type: "caption",
+      title: "Lisa Friedman!!!!!!",
+      text: "top mathlete!!!!"
+    },
+    {
+      type: "caption",
+      title: "Thanks for viewing your Semester Wrapped, see you next semester! ",
+      text: "(Maybe probably not) also dont click off yet theres one more slide ok"
+    },
+    {
+      type: "prompt",
+      text: "Ok bye saide!!!!!"
+    }
+  ],
+  grey: [{
+      type: "prompt",
+      text: "omg hi grey!!"
+    },
+    {
+      type: "answer",
+      text: "hows the fencing been???"
+    },
+    {
+      type: "answer",
+      text: "anyways lets get into it"
+    },
+    {
+      type: "prompt",
+      text: "This semester you talked about the love of your life…"
+    },
+    {
+      type: "answer",
+      text: "So much like so much this is like an all time high"
+    },
+    {
+      type: "prompt",
+      text: "This semester your most awkward moment was…"
+    },
+    {
+      type: "answer",
+      text: "When you gave Ethan a gift but like in the most terrible way possible and then ran away like grey that’s crazy"
+    },
+    {
+      type: "prompt",
+      text: "This semester you were ____ neurodivergent!"
+    },
+    {
+      type: "answer",
+      text: "Totally not at all keep telling urself that"
+    },
+    {
+      type: "prompt",
+      text: "Your top track of this year was…"
+    },
+    {
+      type: "answer",
+      text: "Those misogynist songs which are the exact same song like how is that even possible"
+    },
+    {
+      type: "image",
+      title: "Here’s an image I think describes you:",
+      imageUrl: "https://i.ibb.co/vHJJnsV/IMG-1426.jpg"
+    },
+    {
+      type: "prompt",
+      text: "Your quote of the semester is…"
+    },
+    {
+      type: "caption",
+      title: "Im chill, totally normal, its chill",
+      text: "literally every single time the French teacher walked by"
+    },
+    {
+      type: "prompt",
+      text: "The craziest thing I learned about you this semester is…."
+    },
+    {
+      type: "answer",
+      text: "How much you love this random fencing guy who is seems to be just like a standard guy"
+    },
+    {
+      type: "prompt",
+      text: "This semester your most hated teacher was…"
+    },
+    {
+      type: "caption",
+      title: "Dan Jones",
+      text: "he must inspire the next generation  (jett)"
+    },
+    {
+      type: "prompt",
+      text: "This semester your most loved teacher was…"
+    },
+    {
+      type: "caption",
+      title: "Effy Bently",
+      text: "shocker"
+    },
+    {
+      type: "caption",
+      title: "Thanks for viewing your Semester Wrapped, see you next semester! ",
+      text: "(Maybe probably not) also dont click off yet theres one more slide ok"
+    },
+    {
+      type: "prompt",
+      text: "Ok bye grey hope u like this!!!"
+    }
+  ],
+  rayne: [{
+      type: "prompt",
+      text: "Omg it’s Rayne I can’t believe it!!!"
+    },
+    {
+      type: "answer",
+      text: "Also that lil chocolate bear thing was so good to eat correctly mmmmm"
+    },
+    {
+      type: "answer",
+      text: "ok starting time now"
+    },
+    {
+      type: "prompt",
+      text: "This semester your most awful moment was…"
+    },
+    {
+      type: "answer",
+      text: "When 需老师 yelled at you for like 2 weeks straight to fill to a single form lol"
+    },
+    {
+      type: "prompt",
+      text: "This semester your grades were…."
+    },
+    {
+      type: "answer",
+      text: "Way too good Stop being such a nerd like my gosh"
+    },
+    {
+      type: "prompt",
+      text: "This semester you were ____ neurodivergent!"
+    },
+    {
+      type: "answer",
+      text: "Like good levels of"
+    },
+    {
+      type: "image",
+      title: "Here’s an image I think describes you: (unlike me who def didnt make a website for fun)",
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_5WFgKsdUeeO93dlaLtAwaoMjZ3UPRIorPg&s"
+    },
+    {
+      type: "prompt",
+      text: "Your quote of the semester is…"
+    },
+    {
+      type: "answer",
+      text: "别说话... (not really you saying it but its still iconic)"
+    },
+    {
+      type: "prompt",
+      text: "The craziest thing I learned about you this semester is…."
+    },
+    {
+      type: "answer",
+      text: "That you also do scuba diving!??! Like what how do you have time to do like anything like what"
+    },
+    {
+      type: "prompt",
+      text: "This semester your most hated teacher was…"
+    },
+    {
+      type: "caption",
+      title: "Probably Dan Jones",
+      text: "it would be pretty cool if could grade anything ever and didn’t make fun of women but that’s just me"
+    },
+    {
+      type: "prompt",
+      text: "This semester your most loved teacher was…"
+    },
+    {
+      type: "caption",
+      title: "需老师",
+      text: "she is so silly"
+    },
+    {
+      type: "caption",
+      title: "Thanks for viewing your Semester Wrapped, see you next semester! ",
+      text: "(Maybe probably not) also dont click off yet theres one more slide ok"
+    },
+    {
+      type: "prompt",
+      text: "Ok bye rain!!!!!!"
+    }
+  ],
+  bronwyn: [{
+      type: "prompt",
+      text: "Hi Bronywn :)"
+    },
+    {
+      type: "answer",
+      text: "Do u like the website its pretty cool right"
+    },
+    {
+      type: "answer",
+      text: "anyways starting time now"
+    },
+    {
+      type: "prompt",
+      text: "This semester you talked about the love of your life… (sar sar)"
+    },
+    {
+      type: "answer",
+      text: "Like the amount that grey and azh should but dont"
+    },
+    {
+      type: "prompt",
+      text: "This semester your most awful moment was…"
+    },
+    {
+      type: "answer",
+      text: "Sometime when u yelled something kinda rlly loud srry"
+    },
+    {
+      type: "prompt",
+      text: "This semester your grades were…."
+    },
+    {
+      type: "answer",
+      text: "yippee"
+    },
+    {
+      type: "prompt",
+      text: "This semester you were ____ neurodivergent!"
+    },
+    {
+      type: "answer",
+      text: "yes"
+    },
+    {
+      type: "prompt",
+      text: "Your top track of this year was…"
+    },
+    {
+      type: "answer",
+      text: "The smiths maybe still idk sounds good enough"
+    },
+    {
+      type: "image",
+      title: "Here’s an image I think describes you:",
+      imageUrl: "https://i.redd.it/563c8072tla41.jpg"
+    },
+    {
+      type: "prompt",
+      text: "Your quote of the semester is…"
+    },
+    {
+      type: "caption",
+      title: "*giggles*",
+      text: "Honorable mentions: I know what you are, Yes Diva, The Nerd Store"
+    },
+    {
+      type: "prompt",
+      text: "This semester your most loved teacher was…"
+    },
+    {
+      type: "caption",
+      title: "Robert Wilson & also Sar Sar",
+      text: "tied but for like different reasons"
+    },
+    {
+      type: "caption",
+      title: "Thanks for viewing your Semester Wrapped, see you next semester! ",
+      text: "(Maybe probably not) also dont click off yet theres one more slide ok"
+    },
+    {
+      type: "prompt",
+      text: "Ok bye hope u liked it brownwyebe!!!!!"
+    }
+  ],
+  jett: [{
+      type: "prompt",
+      text: "Lol no you dont get one"
+    },
+    {
+      type: "answer",
+      text: "go back to breaking ur robot"
+    }
+  ],
+};
+let currentCards = [];
+let currentIndex = 0;
+let cardTimer; // Store the timer reference
+
+const namePickerScreen = document.getElementById("name-picker-screen");
+const cardContainer = document.getElementById("card-container");
+const cardElement = document.getElementById("card");
+const cardContent = document.getElementById("card-content");
+const startButton = document.getElementById("start-button");
+const namePicker = document.getElementById("name-picker");
+
+startButton.addEventListener("click", () => {
+  const selectedName = namePicker.value;
+  currentCards = namesData[selectedName];
+  currentIndex = 0;
+
+  namePickerScreen.classList.add("hidden");
+  cardContainer.classList.remove("hidden");
+  loadCard();
+});
+
+cardElement.addEventListener("click", () => {
+  clearTimeout(cardTimer); // Stop the timer when clicked
+  nextCard();
+});
+
+const gradients = ["conic-gradient(#f5adff, #684aff, #6380ff)", // Gradient for card 1
+  "radial-gradient(#ff9a8b, #ff6a88, #ff99ac)", // Gradient for card 2
+  "linear-gradient(#a8ff78, #78ffd6, #85e6ff)", // Gradient for card 3
+  "linear-gradient(135deg, #ff5f6d, #ffc371)", // Bold diagonal blend
+  "radial-gradient(circle at top left, #2af598, #009efd)", // Vibrant corner pop
+  "conic-gradient(from 45deg, #ff7eb3, #ff758c, #8443ff)", // Swirly burst
+  "radial-gradient(ellipse at center, #f5d547, #f09ae9, #5232d9)", // Unexpected trip
+  "linear-gradient(to right, #12c2e9, #c471ed, #f64f59)", // Sleek neon rainbow
+  "radial-gradient(circle, #ff6f91, #f8b5d3, #ffc3a0)", // Sweet pinky tones
+  "linear-gradient(45deg, #ffbc00, #ff0066, #ff3366)", // Hot & spicy
+  "conic-gradient(#e94e77, #b3c2e0, #3d84a8)", // Soft and mysterious
+  "linear-gradient(to top, #a18cd1, #fbc2eb)", // Dreamy lavender-pink
+  "radial-gradient(circle, #89f7fe, #66a6ff)", // Icy blue vibes
+  "linear-gradient(to right, #ff0844, #ffb199)", // Fiery glow
+  "radial-gradient(circle at bottom, #ffecd2, #fcb69f)", // Warm sunrise
+  "linear-gradient(120deg, #f6d365, #fda085)", // Golden hour
+  "conic-gradient(#4facfe, #00f2fe, #4facfe)", // Aqua swirl
+  "linear-gradient(to bottom, #37ecba, #72afd3)", // Fresh minty cool
+  "radial-gradient(circle at center, #fad0c4, #ffd1ff)", // Pastel dreams
+  "linear-gradient(135deg, #ff758c, #ff7eb3, #ffd1dc)", // Rosy sherbet
+  "radial-gradient(ellipse at top, #96fbc4, #f9f586)", // Tropical paradise
+  "linear-gradient(to right, #ee9ca7, #ffdde1)", // Soft candy floss
+  "conic-gradient(from 0deg, #f8ff00, #3ad59f, #3a9ee5)", // Lush spectrum
+  "radial-gradient(circle, #ffafbd, #ffc3a0)", // Blush peachy tones
+  "linear-gradient(45deg, #ff0076, #ff9068, #ffdd93)", // Sunset fire
+  "radial-gradient(circle at center, #fa709a, #fee140)", // Sunrise burst
+  "linear-gradient(to top, #30cfd0, #330867)", // Cool galaxy fade
+  "conic-gradient(from 180deg, #ff9a9e, #fad0c4)", // Light airy swirl
+  "radial-gradient(circle at bottom right, #d4fc79, #96e6a1)", // Fresh garden
+  "linear-gradient(135deg, #5ee7df, #b490ca)", // Aqua lilac wave];
+]
+
+function loadCard() {
+  const cardData = currentCards[currentIndex];
+  cardContent.innerHTML = "";
+
+  // Set card content
+  if (cardData.type === "prompt") {
+    cardContent.innerHTML = `<h1 id="prompt">${cardData.text}</h1>`;
+  } else if (cardData.type === "caption") {
+    cardContent.innerHTML = `<h2>${cardData.title}</h2><p>${cardData.text}</p>`;
+  } else if (cardData.type === "image") {
+    cardContent.innerHTML = `<h3>${cardData.title}</h3><img src="${cardData.imageUrl}" alt="${cardData.title}" style="max-width:100%;border-radius:10px;">`;
+  } else if (cardData.type === "answer") {
+    cardContent.innerHTML = `<h1 id="answer">${cardData.text}</h1>`;
+  }
+
+  // Update background gradient
+  const gradientContainer = document.querySelector(".gradient-container");
+  if (gradientContainer) {
+    gradientContainer.style.background = gradients[Math.floor(Math.random() * gradients.length)];
+  }
+
+  // Apply the "starting" animation for the first and last cards
+  if (currentIndex === 0) {
+    cardElement.classList.add("starting");
+    setTimeout(() => cardElement.classList.remove("starting"), 800); // Remove after animation
+  }
+  if (currentIndex === currentCards.length - 1) {
+    setTimeout(() => {
+      cardElement.classList.add("ending");
+    }, 5000); // Delays adding the "ending" class by 5000ms (5 seconds)
+  } else {
+    cardTimer = setTimeout(nextCard, 6500); // Start or reset the timer for automatic card transition
+  }
+}
+
+function nextCard() {
+  currentIndex++;
+  const blurOverlay = document.getElementById('blur-overlay');
+
+  if (currentIndex < currentCards.length) {
+    blurOverlay.style.opacity = '1'; // Fade in blur
+
+    cardElement.classList.remove("visible");
+    cardElement.classList.add("hidden");
+
+    setTimeout(() => {
+      loadCard();
+      cardElement.classList.remove("hidden");
+      cardElement.classList.add("visible");
+
+      blurOverlay.style.opacity = '0'; // Fade out blur
+    }, 500);
+  } else {
+    blurOverlay.style.opacity = '0'; // Ensure blur is faded out
+    cardContainer.innerHTML = "<h1 id='title'>Thanks for viewing your cards!</h1>";
+  }
+}
+
+const cursor = document.querySelector('.custom-cursor');
+const realcursor = document.querySelector('.realcustom-cursor');
+
+let mouseX = 0,
+  mouseY = 0;
+let cursorX = 0,
+  cursorY = 0;
+
+// Update mouse position
+document.addEventListener('mousemove', (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+// Smooth follow effect
+function animateCursor() {
+  cursorX += (mouseX - cursorX) * 0.1;
+  cursorY += (mouseY - cursorY) * 0.1;
+
+  cursor.style.left = `${cursorX}px`;
+  cursor.style.top = `${cursorY}px`;
+  realcursor.style.left = `${mouseX}px`;
+  realcursor.style.top = `${mouseY}px`;
+
+  requestAnimationFrame(animateCursor);
+}
+
+// Add pop effect
+document.addEventListener('mousemove', () => {
+  cursor.classList.remove('active');
+  clearTimeout(cursor.popTimeout);
+  cursor.popTimeout = setTimeout(() => {
+    cursor.classList.add('active');
+  }, 500);
+});
+
+animateCursor();
